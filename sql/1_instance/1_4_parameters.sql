361d2b4e01863c1d5c7de3c494ab67899bdb4bcf
bazaruzzzero <bazaruzzzero@gmail.com>
2026-02-10 21:18:28 +0300

    basic parameters query

--- sql/1_instance/1_4_parameters.sql
+++ sql/1_instance/1_4_parameters.sql
@@ -1,2 +1,56 @@
--- parameters
-select 'todo';
+-- parameters (basic)
+select
+    name,
+    setting as curr_value,
+    unit,
+    case
+        when name = 'autovacuum' then 'on'
+        when name = 'autovacuum_max_workers' then '3'
+        when name = 'bgwriter_delay' then '50ms / 10ms'
+        when name = 'bgwriter_lru_maxpages' then '300 / 1000'
+        when name = 'checkpoint_completion_target' then '0.9'
+        when name = 'checkpoint_timeout' then '900s'
+        when name = 'fastpath_num_locks' then '-'
+        when name = 'log2_num_lock_partitions' then '-'
+        when name = 'max_wal_size' then '-'
+        when name = 'plan_cache_mode' then 'force_custom_plan'
+        when name = 'synchronous_commit' then 'on'
+        when name = 'wal_compression' then 'on / lz4'
+        when name = 'wal_sync_method' then 'fdatasync'
+        when name = 'shared_buffers' then '25-50% RAM'
+        when name = 'effective_cache_size' then '75% RAM'
+        when name = 'huge_pages' then 'on'
+        when name = 'max_parallel_workers_per_gather' then '0'
+        when name = 'max_parallel_maintenance_workers' then '-'
+        when name = 'max_parallel_workers' then '-'
+        when name = 'max_worker_processes' then '-'
+        when name = 'track_io_timing' then 'on'
+        when name = 'autovacuum_timeout_threshold_enable' then 'on'
+        else '-'
+    end as recom_value
+from 
+    pg_settings
+where
+    name in ('autovacuum'
+    'autovacuum_max_workers',
+    'bgwriter_delay',
+    'bgwriter_lru_maxpages',
+    'checkpoint_completion_target',
+    'checkpoint_timeout',
+    'fastpath_num_locks',
+    'log2_num_lock_partitions',
+    'max_wal_size',
+    'plan_cache_mode',
+    'synchronous_commit',
+    'wal_compression',
+    'wal_sync_method',
+    'shared_buffers',
+    'effective_cache_size',
+    'huge_pages',
+    'max_parallel_workers_per_gather',
+    'max_parallel_maintenance_workers',
+    'max_parallel_workers',
+    'max_worker_processes',
+    'track_io_timing',
+    'autovacuum_timeout_threshold_enable')
+order by name;
